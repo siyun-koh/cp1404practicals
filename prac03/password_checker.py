868b3f7f0f8657f1f83c6d151e71dd33f173cb21
siyun-koh <siyun.kon@my.jcu.edu.au>
2019-11-28 16:14:50 +0800

    updated password checker

--- prac03/password_checker.py
+++ prac03/password_checker.py
@@ -1,9 +1,30 @@
-passwordMinLength = 8
+# without function
+MIN_PASS_LENGTH = 8
 
-password = input("Please enter a password of minimum {} characters: ".format(passwordMinLength))
-while len(password) < passwordMinLength:
+"""password = input("Please enter a password of minimum {} characters: ".format(MIN_PASS_LENGTH))
+while len(password) < MIN_PASS_LENGTH:
     print("Your password is too short. Please enter a new one.")
-    password = input("Please enter a password of minimum {} characters: ".format(passwordMinLength))
+    password = input("Please enter a password of minimum {} characters: ".format(MIN_PASS_LENGTH))
+    
+    print('*' * len(password))
+"""
 
-print('*' * len(password))
 
+def main():
+    password = get_password(MIN_PASS_LENGTH)
+    print_asterisk(password)
+
+
+def get_password(min_pass_length):
+    password = input("Please enter a password of minimum {} characters: ".format(MIN_PASS_LENGTH))
+    while len(password) < MIN_PASS_LENGTH:
+        print("Your password is too short. Please enter a new one.")
+        password = input("Please enter a password of minimum {} characters: ".format(MIN_PASS_LENGTH))
+    return password
+
+
+def print_asterisk(sequence):
+    print('*' * len(sequence))
+
+
+main()
