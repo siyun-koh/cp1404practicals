d626624c8c28b21f32ae4b650020d4421cb1e673
siyun-koh <siyun.kon@my.jcu.edu.au>
2019-12-12 15:51:03 +0800

    state_names

--- prac05/states_names.py
+++ prac05/states_names.py
@@ -4,7 +4,6 @@ State names in a dictionary
 File needs reformatting
 """
 
-# TODO: Reformat this file so the dictionary code follows PEP 8 convention
 STATE_NAMES = {"QLD": "Queensland",
                "NSW": "New South Wales",
                "NT": "Northern Territory",
