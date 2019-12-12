0725941ecab03e1b42a313b83ca5a884d6ce1c2b
siyun-koh <siyun.kon@my.jcu.edu.au>
2019-12-12 16:40:14 +0800

    updated hex_colours

--- prac05/hex_colours.py
+++ prac05/hex_colours.py
@@ -1,15 +1,15 @@
-HEX_COLOUR_CODES = {"AliceBlue": "#f0f8ff",
-                    "BlueViolet": "#8a2be2",
-                    "CadetBlue1": "#98f5ff",
-                    "CornflowerBlue": "#6495ed",
-                    "DarkGreen": "#006400",
-                    "DarkOrchid": "#9932cc",
+HEX_COLOUR_CODES = {"aliceblue": "#f0f8ff",
+                    "blueviolet": "#8a2be2",
+                    "cadetblue1": "#98f5ff",
+                    "cornflowerblue": "#6495ed",
+                    "darkgreen": "#006400",
+                    "darkorchid": "#9932cc",
                     "firebrick": "#b22222",
-                    "FloralWhite": "#fffaf0",
-                    "IndianRed1": "	#ff6a6a",
+                    "floralwhite": "#fffaf0",
+                    "indianred1": "	#ff6a6a",
                     "khaki1": "#fff68f"}
 
-colour_name = input("Enter a colour name: ")
+colour_name = input("Enter a colour name: ").lower()
 while colour_name != "":
     print("The code for colour name \"{}\" is: {}".format(colour_name, HEX_COLOUR_CODES.get(colour_name)))
-    colour_name = input("Enter a colour name: ")
+    colour_name = input("Enter a colour name: ").lower()
