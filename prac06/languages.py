2ba4a8d59e4e46eeb67d271ca5b969e990e62fb0
siyun-koh <siyun.kon@my.jcu.edu.au>
2019-12-19 16:28:00 +0800

    languages

---
+++ prac06/languages.py
@@ -0,0 +1,14 @@
+from prac06.programming_language import ProgrammingLanguage
+
+
+def main():
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+    print(ruby)
+    print(python)
+    print(visual_basic)
+
+
+main()
---
+++ prac06/programming_language.py
@@ -0,0 +1,18 @@
+class ProgrammingLanguage:
+    """Represents Programming Language object"""
+    def __init__(self, name, typing, reflection, year):
+        """Initialise language instance"""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def __str__(self):
+        """returns string representative of object"""
+        return "{}, {} Typing, Reflection = {}, First appeared in {}".format(self.name, self.typing, self.reflection,
+                                                                             self.year)
+
+    def is_dynamic(self):
+        """Checks if language is dynamically typed"""
+        return self.dynamic == "Dynamic"
+
