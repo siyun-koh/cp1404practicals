db6809aa478ef9aa553e2c4d94326854195ecda6
siyun-koh <siyun.kon@my.jcu.edu.au>
2020-01-09 15:18:57 +0800

    prac 08

---
+++ prac08/car.py
@@ -0,0 +1,37 @@
+"""CP1404/CP5632 Practical - Car class example."""
+
+
+class Car:
+    """Represent a Car object."""
+
+    def __init__(self, fuel=0, name="null"):
+        """Initialise a Car instance.
+
+        fuel: float, one unit of fuel drives one kilometre
+        """
+        self.fuel = fuel
+        """name: string for car name"""
+        self.name = name
+        self.odometer = 0
+
+    def __str__(self):
+        """Returns string representing Car object"""
+        return "{}, Fuel = {}, Odometer = {}".format(self.name, self.fuel, self.odometer)
+
+    def add_fuel(self, amount):
+        """Add amount to the car's fuel."""
+        self.fuel += amount
+
+    def drive(self, distance):
+        """Drive the car a given distance.
+
+        Drive given distance if car has enough fuel
+        or drive until fuel runs out return the distance actually driven.
+        """
+        if distance > self.fuel:
+            distance = self.fuel
+            self.fuel = 0
+        else:
+            self.fuel -= distance
+        self.odometer += distance
+        return distance
